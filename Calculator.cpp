7851d12a3e423d35ec63c7f0501a64c7b96ceaf5
ManZ988 <167453276+ManZ988@users.noreply.github.com>
2024-05-01 19:19:32 +0300

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,28 +4,22 @@ using namespace std;
 
 int main() {
     double num1, num2;
-    char operation;
+    string operation;
     cout << "Введите первое число, операцию и второе число (например, 5 + 3): ";
     cin >> num1 >> operation >> num2;
 
-    switch (operation) {
-    case '+':
+    if (operation == "+") 
         cout << "Результат: " << num1 + num2 << endl;
-        break;
-    case '-':
+    else if (operation == "-")
         cout << "Результат: " << num1 - num2 << endl;
-        break;
-    case '*':
+    else if (operation == "*")
         cout << "Результат: " << num1 * num2 << endl;
-        break;
-    case '/':
+    else if (operation == "/")
         if (num2 != 0.0)
             cout << "Результат: " << num1 / num2 << endl;
         else
             cout << "Ошибка: Деление на ноль!" << endl;
-        break;
-    default:
+    else
         cout << "Ошибка: Неподдерживаемая операция!" << endl;
-    }
 
 }
